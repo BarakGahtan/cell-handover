2672cb6a56663f075ceacb192048cadae5be1294
barakgahtan <barakgahtan@campus.technion.ac.il>
2023-01-14 14:34:26 +0200

    working

--- cell_calculation.py
+++ cell_calculation.py
@@ -6,7 +6,7 @@ def where_are_my_cells(cell_num):
     cell_location_israel['cell'] = cell_location_israel['cell'].astype('str')
     mask = cell_location_israel['cell'].str.startswith(str(cell_num))
     result = cell_location_israel[mask].drop('radio', axis=1).astype(float)
-    result = pd.DataFrame(result.mean()).transpose()  # average for cell
+    # result = pd.DataFrame(result.mean()).transpose()  # average for cell
     if result.empty:
         mask = cell_location_israel['cell'].str.contains(str(cell_num))
         result = cell_location_israel[mask]
@@ -17,6 +17,7 @@ def where_are_my_cells(cell_num):
 def get_unique_cells_in_drive(df):
     unique_cells_in_this_drive = df['globalcellid'].unique()  # unique cells from the data-frame
     cell_dfs_list = []
+    cell_name = ''
     for cell in unique_cells_in_this_drive:
         if isinstance(cell, float):
             if cell == 0:
@@ -35,7 +36,7 @@ def get_unique_cells_in_drive(df):
 
 
 def calculate_switchover(df):
-    old_global_cell = df.head(1).globalcellid[0]
+    old_global_cell = df.head(1).globalcellid.values[0]
     current_global_cel = -1
     for idx, row in df.iterrows():
         if df.loc[idx, 'globalcellid'] == old_global_cell:
@@ -43,4 +44,3 @@ def calculate_switchover(df):
         if df.loc[idx, 'globalcellid'] != old_global_cell and df.loc[idx, 'globalcellid'] != 0:
             df.loc[idx, 'switchover'] = 1
             old_global_cell = df.loc[idx, 'globalcellid']
-            x = 5
--- cell_hadover_pred.py
+++ cell_hadover_pred.py
@@ -9,39 +9,48 @@ import cell_calculation
 from aux_functions import euclidean_distance
 from cell_calculation import get_unique_cells_in_drive
 
-NUM_DRIVES = 2
-picke_name = 'pickle_rick.pkl'
-big_df = pd.read_pickle(picke_name)
+NUM_DRIVES = 1
+DRIVE_NUM = 400
+pickle_name = 'pickle_rick.pkl'
+big_df = pd.read_pickle(pickle_name)
 drives = [v for k, v in big_df.groupby(['date', 'time'])]  # filter according to start of the drive. unique.
 
 # Now filter according to modem in each drive
 drives_by_modem = []
 for i in range(len(drives)):
-    drives_by_modem.append([v for k, v in drives[i].groupby('modem_id')])
+    drives_by_modem.append([v for k, v in drives[i].groupby('imei')])
 # drive_by_modems is list of lists. each drive is a list of lists. the list inside is for diff modems.
 
 # each drive is broken into 5-6 modems. There are 600 drives and at most 6 modems.
-drives_by_modem_dict = {}
-for i in range(NUM_DRIVES):  # len(drives_by_modem)
-    for j in range(len(drives_by_modem[i])):  # len(drives_by_modem[i]) - number os modems in drive.
-        key_for_dict = str(drives_by_modem[i][j]['date'].iloc[0] + '_' + drives_by_modem[i][j]['time'].iloc[0] + '_' +
-                           drives_by_modem[i][j]['modem_id'].iloc[0])
-        drives_by_modem_dict[key_for_dict] = drives_by_modem[i][j]
+drives_by_imei_dict = {}
+# for i in range(NUM_DRIVES):  # len(drives_by_modem)
+for j in range(len(drives_by_modem[DRIVE_NUM])):  # len(drives_by_modem[i]) - number os modems in drive.
+    key_for_dict = str(
+        drives_by_modem[DRIVE_NUM][j]['date'].iloc[0] + '_' + drives_by_modem[DRIVE_NUM][j]['time'].iloc[0] + '_' +
+        str(drives_by_modem[DRIVE_NUM][j]['imei'].iloc[0]))
+    drives_by_imei_dict[key_for_dict] = drives_by_modem[DRIVE_NUM][j]
 
 # identifying the cells of each drive per modem. the key of the dict is when the drive started following by the
 # modem_id. key=date_time_modemID
 cells_per_drive_per_modem_avg = {}
-for key in drives_by_modem_dict.keys():  # len(drives_by_modem)
-    cells_per_drive_per_modem_avg[key] = pd.concat(get_unique_cells_in_drive(drives_by_modem_dict[key]), axis=0,
+for key in drives_by_imei_dict.keys():  # len(drives_by_modem)
+    cells_per_drive_per_modem_avg[key] = pd.concat(get_unique_cells_in_drive(drives_by_imei_dict[key]), axis=0,
                                                    join='outer').dropna(axis=0)
+
+for key in drives_by_imei_dict.keys():
+    drives_by_imei_dict[key].insert(18, "switchover", 0, allow_duplicates=False)
+    drives_by_imei_dict[key].insert(18, "globacellid_shift", 0, allow_duplicates=False)
+    drives_by_imei_dict[key]['globacellid_shift'] = drives_by_imei_dict[key]['globalcellid'].shift()
+    drives_by_imei_dict[key]['globalcellid'] = drives_by_imei_dict[key].apply(
+        lambda x: x['globacellid_shift'] if x['globalcellid'] == 0 else x['globalcellid'], axis=1)
+    cell_calculation.calculate_switchover(drives_by_imei_dict[key])  # Calculate switch over per drive per imei
 
 # Create a map object
 map_object = folium.Map(location=[32.17752, 34.93073], zoom_start=15)
 
-for key in drives_by_modem_dict.keys():
+for key in drives_by_imei_dict.keys():
     locations_in_drive_per_modem = cells_per_drive_per_modem_avg[key][['lat', 'lon']]
     locations_to_list = locations_in_drive_per_modem.values.tolist()
-    unique_IMEIs_per_drive_per_modem = drives_by_modem_dict[key]['imei'].unique()
     modems_holder, stat_holder = {}, {}
     map_speed_for_drive_for_modem = folium.Map(location=[32.17752, 34.93073], zoom_start=25)
     map_switchover_for_drive_for_modem = folium.Map(location=[32.17752, 34.93073], zoom_start=25)
@@ -51,81 +60,66 @@ for key in drives_by_modem_dict.keys():
     # making the cells according to range
     all_cells_df = cells_per_drive_per_modem_avg[key].reset_index()
     for index, row in all_cells_df.iterrows():
-        folium.Circle((row['lat'], row['lon']), radius=row['range'], fill=True, opacity=0.9, weight=2).add_to(map_switchover_for_drive_for_modem)
-    for imei in unique_IMEIs_per_drive_per_modem:
-        modems_holder[imei] = drives_by_modem_dict[key][drives_by_modem_dict[key]['imei'] == imei]
-        # modems_holder[imei].insert(18, "switchover", 0, allow_duplicates=False)
-        # modems_holder[imei].replace(999999, np.nan, inplace=True)
-        # modems_holder[imei].ffill(inplace=True)
-        # modems_holder[imei].bfill(inplace=True)
-        # modems_holder[imei]['globacellid_shift'] = modems_holder[imei]['globalcellid'].shift()
-        # modems_holder[imei]['switchover'] = modems_holder[imei].apply(
-        #     lambda x: 0 if x['globalcellid'] == x['globacellid_shift'] else 1, axis=1)
-        modems_holder[imei]['latitude_perimeter_shift'] = modems_holder[imei]['latitude_perimeter'].shift()
-        modems_holder[imei]['longitude_perimeter_shift'] = modems_holder[imei]['longitude_perimeter'].shift()
-        # Calculate the Euclidean distance between the two points for each row
-        modems_holder[imei]['distance'] = modems_holder[imei].apply(euclidean_distance, axis=1)
-        modems_holder[imei]['speed'] = modems_holder[imei]['distance'] * 60 * 60 * 111 * -1
-        modems_holder[imei].fillna(0, inplace=True)
-        cell_calculation.calculate_switchover(modems_holder[imei])
+        folium.Circle((row['lat'], row['lon']), radius=row['range'], fill=True, opacity=0.9, weight=2).add_to(
+            map_switchover_for_drive_for_modem)
 
-        colormap = cm.linear.RdYlGn_11.scale(0, 35).to_step(20)
-        colormap.caption = 'Green is greater velocity'
-        map_speed_for_drive_for_modem.add_child(colormap)
-        data1 = modems_holder[imei][['latitude_perimeter', 'longitude_perimeter', 'speed']]
-        data_filt1 = data1.values.tolist()
-        HeatMap(data=data_filt1, use_local_extrema=False, min_opacity=0.5, max_opacity=0.95, radius=15) \
-            .add_to(folium.FeatureGroup(name=str(imei) + ' speed').add_to(map_speed_for_drive_for_modem))
+    drives_by_imei_dict[key]['latitude_perimeter_shift'] = drives_by_imei_dict[key]['latitude_perimeter'].shift()
+    drives_by_imei_dict[key]['longitude_perimeter_shift'] = drives_by_imei_dict[key]['longitude_perimeter'].shift()
+    # Calculate the Euclidean distance between the two points for each row
+    drives_by_imei_dict[key]['distance'] = drives_by_imei_dict[key].apply(euclidean_distance, axis=1)
+    drives_by_imei_dict[key]['speed'] = drives_by_imei_dict[key]['distance'] * 60 * 60 * 111 * -1
+    drives_by_imei_dict[key].fillna(0, inplace=True)
 
-        # data2 = modems_holder[imei][['latitude_perimeter', 'longitude_perimeter', 'switchover']].groupby([
-        # 'latitude_perimeter', 'longitude_perimeter']).mean().reset_index()
-        data2 = modems_holder[imei][['latitude_perimeter', 'longitude_perimeter', 'switchover']]
-        data2 = data2[data2['switchover'] == 1]
-        marker_cluster = MarkerCluster(name=str(imei) + ' cluster').add_to(map_switchover_for_drive_for_modem)
-        for index, row in data2.iterrows():
-            folium.Marker(location=[row['latitude_perimeter'], row['longitude_perimeter']],
-                          popup=row['switchover']).add_to(marker_cluster)
+    colormap = cm.linear.RdYlGn_11.scale(0, 35).to_step(20)
+    colormap.caption = 'Green is greater velocity'
+    map_speed_for_drive_for_modem.add_child(colormap)
+    data1 = drives_by_imei_dict[key][['latitude_perimeter', 'longitude_perimeter', 'speed']]
+    data_filt1 = data1.values.tolist()
+    HeatMap(data=data_filt1, use_local_extrema=False, min_opacity=0.5, max_opacity=0.95, radius=15) \
+        .add_to(folium.FeatureGroup(name=str(key) + ' speed').add_to(map_speed_for_drive_for_modem))
 
-        data_filt2 = data2.values.tolist()
-        HeatMap(data=data_filt2, use_local_extrema=False, min_opacity=0.0,max_opacity=0.95,radius=10).add_to(
-            folium.FeatureGroup(name=str(imei) + ' switchover').add_to(map_switchover_for_drive_for_modem))
+    data2 = drives_by_imei_dict[key][['latitude_perimeter', 'longitude_perimeter', 'switchover']]
+    data2 = data2[data2['switchover'] == 1]
+    marker_cluster = MarkerCluster(name=str(key) + ' cluster').add_to(map_switchover_for_drive_for_modem)
+    for index, row in data2.iterrows():
+        folium.Marker(location=[row['latitude_perimeter'], row['longitude_perimeter']],
+                      popup=row['switchover']).add_to(marker_cluster)
 
-        # data3 = modems_holder[imei][['latitude_perimeter', 'longitude_perimeter', 'loss_rate']].groupby([
-        # 'latitude_perimeter', 'longitude_perimeter']).mean().reset_index()
-        data3 = modems_holder[imei][['latitude_perimeter', 'longitude_perimeter', 'loss_rate']]
-        data3 = data3[data3['loss_rate'] > 0]
+    data_filter2 = data2.values.tolist()
+    HeatMap(data=data_filter2, use_local_extrema=False, min_opacity=0.0, max_opacity=0.95, radius=10).add_to(
+        folium.FeatureGroup(name=str(key) + ' switchover').add_to(map_switchover_for_drive_for_modem))
 
-        marker_cluster_loss = MarkerCluster(name=str(imei) + ' cluster').add_to(map_loss_for_drive_for_modem)
-        for index, row in data3.iterrows():
-            folium.Marker(location=[row['latitude_perimeter'], row['longitude_perimeter']],
-                          popup=row['loss_rate']).add_to(marker_cluster_loss)
-        data_filt3 = data3.values.tolist()
-        HeatMap(data=data_filt3, use_local_extrema=False, min_opacity=0.5,
-                max_opacity=0.9,
-                radius=25).add_to(folium.FeatureGroup(name=str(imei) + ' loss').add_to(map_loss_for_drive_for_modem))
+    data3 = drives_by_imei_dict[key][['latitude_perimeter', 'longitude_perimeter', 'loss_rate']]
+    data3 = data3[data3['loss_rate'] > 0]
+    marker_cluster_loss = MarkerCluster(name=str(key) + ' cluster').add_to(map_loss_for_drive_for_modem)
+    for index, row in data3.iterrows():
+        folium.Marker(location=[row['latitude_perimeter'], row['longitude_perimeter']],
+                      popup=row['loss_rate']).add_to(marker_cluster_loss)
+    data_filt3 = data3.values.tolist()
+    HeatMap(data=data_filt3, use_local_extrema=False, min_opacity=0.5,
+            max_opacity=0.9,
+            radius=25).add_to(folium.FeatureGroup(name=str(key) + ' loss').add_to(map_loss_for_drive_for_modem))
 
-        # data4 = modems_holder[imei][['latitude_perimeter', 'longitude_perimeter', 'rsrp']].groupby([
-        # 'latitude_perimeter', 'longitude_perimeter']).mean().reset_index()
-        data4 = modems_holder[imei][['latitude_perimeter', 'longitude_perimeter', 'rsrp']]
-        data_filt4 = data4.values.tolist()
-        HeatMap(data=data_filt4, use_local_extrema=False, min_opacity=0.5,
-                max_opacity=0.9,
-                radius=25).add_to(folium.FeatureGroup(name=str(imei) + ' rsrp').add_to(map_rsrp_for_drive_for_modem))
+    data4 = drives_by_imei_dict[key][['latitude_perimeter', 'longitude_perimeter', 'rsrp']]
+    data_filt4 = data4.values.tolist()
+    HeatMap(data=data_filt4, use_local_extrema=False, min_opacity=0.5,
+            max_opacity=0.9,
+            radius=25).add_to(folium.FeatureGroup(name=str(key) + ' rsrp').add_to(map_rsrp_for_drive_for_modem))
 
-        corr_mat = modems_holder[imei].corr()
-        corr_mat.to_csv(str(imei) + '_corr_mat.csv')
-        stat_holder[imei] = modems_holder[imei].describe()[['switchover', 'loss_rate', 'rsrp', 'latency_mean']].loc[
-            'mean']
-        # corr_mat = modems_holder[imei].describe()
-        # corr_mat.to_csv(str(imei) + '_stats.csv')
-        folium.LayerControl().add_to(map_speed_for_drive_for_modem)
-        map_speed_for_drive_for_modem.save(key + '-speed.html')
-        folium.LayerControl().add_to(map_switchover_for_drive_for_modem)
-        map_switchover_for_drive_for_modem.save(key + '-switchover.html')
-        folium.LayerControl().add_to(map_loss_for_drive_for_modem)
-        map_loss_for_drive_for_modem.save(key + '-loss.html')
-        folium.LayerControl().add_to(map_rsrp_for_drive_for_modem)
-        map_rsrp_for_drive_for_modem.save(key + '-rsrp.html')
-        pd.DataFrame.from_dict(stat_holder).to_csv(key + '-stats_compare.csv')
-        x = 5
+    # corr_mat = modems_holder[imei].corr()
+    # corr_mat.to_csv(str(imei) + '_corr_mat.csv')
+    # stat_holder[imei] = modems_holder[imei].describe()[['switchover', 'loss_rate', 'rsrp', 'latency_mean']].loc[
+    # 'mean']
+    # corr_mat = modems_holder[imei].describe()
+    # corr_mat.to_csv(str(imei) + '_stats.csv')
+    folium.LayerControl().add_to(map_speed_for_drive_for_modem)
+    map_speed_for_drive_for_modem.save(key + '-speed.html')
+    folium.LayerControl().add_to(map_switchover_for_drive_for_modem)
+    map_switchover_for_drive_for_modem.save(key + '-switchover.html')
+    folium.LayerControl().add_to(map_loss_for_drive_for_modem)
+    map_loss_for_drive_for_modem.save(key + '-loss.html')
+    folium.LayerControl().add_to(map_rsrp_for_drive_for_modem)
+    map_rsrp_for_drive_for_modem.save(key + '-rsrp.html')
+    # pd.DataFrame.from_dict(stat_holder).to_csv(key + '-stats_compare.csv')
+    x = 5
 # print(drive_df)
