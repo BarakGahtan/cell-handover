9b83622c5956bf3dcd80f082714a5e1052d8bec2
barakgahtan <barakgahtan@campus.technion.ac.il>
2023-01-21 09:36:02 +0200

    feature selection working based on correlation

--- data_loader.py
+++ data_loader.py
@@ -1,28 +1,12 @@
 import copy
-import os
-from collections import defaultdict
-import folium as folium
-from folium.plugins import HeatMap, MarkerCluster
+
 import numpy as np
 import pandas as pd
-import branca.colormap as cm
+from matplotlib import pyplot as plt
+
 import cell_calculation
-from aux_functions import euclidean_distance, euclidean_distance_points, haversine, getDistanceBetweenPointsNew
-from cell_calculation import get_unique_cells_in_drive
-from regression_model import build_regression_model
-import matplotlib.pyplot as mp
-from sklearn import preprocessing
+from main import DRIVE_NUM, NUM_DRIVES
 import seaborn as sns
-import matplotlib.pyplot as plt
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-import warnings
-
-warnings.filterwarnings("ignore")
-from visualization import prepare_distance_to_cells
-
-# filter according to start of the drive. unique.
-NUM_DRIVES = 1
-DRIVE_NUM = 400
 
 
 def init_dataset(pickle_name, drive_num):
@@ -51,7 +35,7 @@ def init_dataset(pickle_name, drive_num):
 def get_cells_per_drive_in_dataset(drives_by_imei_dictionary):
     cells_per_drive_per_modem_avg = {}
     for key in drives_by_imei_dictionary.keys():  # len(drives_by_modem)
-        cells, updated_df = get_unique_cells_in_drive(drives_by_imei_dictionary[key])
+        cells, updated_df = cell_calculation.get_unique_cells_in_drive(drives_by_imei_dictionary[key])
         cells_per_drive_per_modem_avg[key] = pd.concat(cells, axis=0, join='outer').dropna(axis=0)
         drives_by_imei_dictionary[key] = updated_df
     cells_dictionary = {}
@@ -81,11 +65,10 @@ def prepare_switchover_col(drives_by_imei_dictionary):
         drives_by_imei_dictionary[key].drop(["global_cell_id_shift"], axis=1, inplace=True)
         cell_calculation.calculate_switchover(
             drives_by_imei_dictionary[key])  # Calculate switch over per drive per imei
-
     return drives_by_imei_dictionary
 
 
-def prepare_features(data_dict):
+def normalize_correlate_features(data_dict):
     labels_dict = {}
     for key in data_dict.keys():
         data_dict[key].drop(
@@ -101,23 +84,24 @@ def prepare_features(data_dict):
                 data_dict[key][normalized_cols].max() - data_dict[key][normalized_cols].min())
         data_cols = ['longitude', 'latitude', 'rsrp', 'rssi', 'rsrq', 'modem_bandwidth', 'latency_mean',
                      'total_bitrate', 'frame_latency_mean', 'timestamp', 'globalcellid', 'celldecimal', 'loss_rate',
-                     'qp_mean', 'switchover_decimal', 'switchover_global']
+                     'qp_mean', 'switchover_global']
         labels_dict[key] = copy.copy(data_dict[key][data_cols])
         data_dict[key] = copy.copy(data_dict[key][data_cols])
         corr = data_dict[key].corr()
+        selected_columns = np.full((corr.shape[0],), True, dtype=bool)
+        for i in range(corr.shape[0]):
+            for j in range(i + 1, corr.shape[0]):
+                if corr.iloc[i, j] >= 0.9:
+                    if selected_columns[j]:
+                        selected_columns[j] = False
+        selected_columns = data_dict[key].columns[selected_columns]
+        data_dict[key] = copy.copy(data_dict[key][selected_columns])
         sns.heatmap(corr, fmt=".2f", linewidth=.2)
-        plt.rcParams["figure.figsize"] = (15, 15)
+        plt.title("Communication Test Drive Features")
+        plt.rcParams["figure.figsize"] = (30, 30)
+        # plt.xticks(fontsize=16)
+        plt.tight_layout()
+        # plt.savefig("correlationmat.pdf", dpi=300)
+        x = 5
         # plt.show(dpi=300)
-        plt.savefig("correlationmat.pdf", dpi=300)
-    return data_dict, labels_dict
-
-
-if __name__ == "__main__":
-    drives_by_modem, returned_drives_by_imei_dict = init_dataset('pickle_rick.pkl', DRIVE_NUM)
-    cells_per_drives_in_dataset, cells_dict = get_cells_per_drive_in_dataset(returned_drives_by_imei_dict)
-    drives_by_imei_dict = prepare_switchover_col(returned_drives_by_imei_dict)
-    prepare_features(drives_by_imei_dict)
-    x = 5
-    # drives_by_imei_dict, rsrp_dictionary = prepare_distance_to_cells(drives_by_imei_dict, cells_dict)
-    # visualize_drives(returned_drives_by_imei_dict, cells_per_drives_in_dataset)
-    build_regression_model(drives_by_imei_dict)
+    return data_dict
--- main.py
+++ main.py
@@ -1,9 +1,37 @@
+import copy
+import os
+from collections import defaultdict
+import folium as folium
+from folium.plugins import HeatMap, MarkerCluster
+import numpy as np
 import pandas as pd
+import branca.colormap as cm
+import cell_calculation
+from aux_functions import euclidean_distance, euclidean_distance_points, haversine, getDistanceBetweenPointsNew
+from cell_calculation import get_unique_cells_in_drive
+from data_loader import init_dataset, get_cells_per_drive_in_dataset, prepare_switchover_col, \
+    normalize_correlate_features
+from regression_model import build_regression_model
+import matplotlib.pyplot as mp
+from sklearn import preprocessing
 
-def cells_reader():
-    opencell_id_data = pd.read_csv("425.csv")
-    obj = pd.read_pickle(r'/Users/barakgahtan/PycharmProjects/Operator-Selector/pickle_rick.pkl')
-    x=5
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+import warnings
+
+warnings.filterwarnings("ignore")
+from visualization import prepare_distance_to_cells
+
+# filter according to start of the drive. unique.
+NUM_DRIVES = 1
+DRIVE_NUM = 400
 
 if __name__ == "__main__":
-    cells_reader()
+    drives_by_modem, returned_drives_by_imei_dict = init_dataset('pickle_rick.pkl', DRIVE_NUM)
+    cells_per_drives_in_dataset, cells_dict = get_cells_per_drive_in_dataset(returned_drives_by_imei_dict)
+    drives_by_imei_dict = prepare_switchover_col(returned_drives_by_imei_dict)
+    correlated_data_dict = normalize_correlate_features(drives_by_imei_dict)
+
+    # drives_by_imei_dict, rsrp_dictionary = prepare_distance_to_cells(drives_by_imei_dict, cells_dict)
+    # visualize_drives(returned_drives_by_imei_dict, cells_per_drives_in_dataset)
+    build_regression_model(drives_by_imei_dict)
