f2012e66f7533714b8f2b512f63fe663721cafd6
barak <barakgahtan@gmail.com>
2023-01-30 10:10:27 +0200

    data is balanced with SMOTE

--- training_model.py
+++ training_model.py
@@ -1,42 +1,70 @@
 # Measure our neural network by mean square error
 import copy
 
+import numpy as np
 import torch
 from torch import optim
 
 from load_drives import create_seq
+from imblearn.over_sampling import SMOTE
 
 
 def make_Tensor(array):
     # return torch.Tensor(array).float()
     return torch.from_numpy(array).float()
 
+
 def balance_data_set(seq, seq_label):
-    count_label_0, count_label_1 = 0,0
-    seq_copy = copy.copy(seq[:2])
-    seq_copy_label = copy.copy(seq_label[:2])
+    count_label_0, count_label_1 = 0, 0
+    minority, majority = [], []
+    minority_label, majority_label = [], []
     for i in range(len(seq)):
         time_series = seq[i]
         time_series_label = seq_label[i]
         if time_series_label == 0:
             count_label_0 = count_label_0 + 1
+            majority.append(seq[i])
+            majority_label.append(seq_label[i])
         else:
             count_label_1 = count_label_1 + 1
+            minority.append(seq[i])
+            minority_label.append(seq_label[i])
+    sm = SMOTE(sampling_strategy='minority', random_state=42)
+    seq_data_reshaped_for_balancing = seq.reshape(seq.shape[0], -1)
+    oversampled_seq, oversampled_labels = sm.fit_resample(seq_data_reshaped_for_balancing, seq_label)
+    oversampled_seq = oversampled_seq.reshape(oversampled_seq.shape[0], seq.shape[1], seq.shape[2])
+    return oversampled_seq, oversampled_labels
+    # minority = np.array(minority)
+    # minority = minority.reshape(minority.shape[0],-1)
+    # minority_label = np.array(minority_label)
+    # minority_label = minority_label.reshape(minority_label.shape[0],-1)
+    # majority = np.array(majority)
+    # majority = majority.reshape(majority.shape[0],-1)
+    # majority_label = np.array(majority_label)
+    # majority_label = majority_label.reshape(majority_label.shape[0],-1)
 
-    #
-    # return balanced_seq, balanced_seq_label
 
-def prepare_data_sets(data_frame, SEQ_LEN):
+def prepare_data_sets(data_frame, SEQ_LEN, balanced):
     seq, seq_label = create_seq(data_frame, SEQ_LEN)
-    seq, seq_label = balance_data_set(seq, seq_label)
-    data_set_size = seq.shape[0]
-    train_size = int(data_set_size * 0.8)
-    test_size = int(int(data_set_size - train_size) / 2)
-    x_train, y_train = copy.copy(seq[:train_size]), copy.copy(seq_label[:train_size])
-    X_val, y_val = copy.copy(seq[train_size:train_size + test_size]), copy.copy(
-        seq_label[train_size:train_size + test_size])
-    X_test, y_test = copy.copy(seq[train_size + test_size:]), copy.copy(
-        seq_label[train_size + test_size:])
+    oversampled_seq, oversampled_labels = balance_data_set(seq, seq_label)
+    if balanced:
+        data_set_size = oversampled_seq.shape[0]
+        train_size = int(data_set_size * 0.8)
+        test_size = int(int(data_set_size - train_size) / 2)
+        x_train, y_train = copy.copy(oversampled_seq[:train_size]), copy.copy(oversampled_labels[:train_size])
+        X_val, y_val = copy.copy(oversampled_seq[train_size:train_size + test_size]), copy.copy(
+            oversampled_labels[train_size:train_size + test_size])
+        X_test, y_test = copy.copy(oversampled_seq[train_size + test_size:]), copy.copy(
+            oversampled_labels[train_size + test_size:])
+    else:
+        data_set_size = seq.shape[0]
+        train_size = int(data_set_size * 0.8)
+        test_size = int(int(data_set_size - train_size) / 2)
+        x_train, y_train = copy.copy(seq[:train_size]), copy.copy(seq_label[:train_size])
+        X_val, y_val = copy.copy(seq[train_size:train_size + test_size]), copy.copy(
+            seq_label[train_size:train_size + test_size])
+        X_test, y_test = copy.copy(seq[train_size + test_size:]), copy.copy(
+            seq_label[train_size + test_size:])
     return make_Tensor(x_train), make_Tensor(y_train), make_Tensor(X_val), make_Tensor(y_val), make_Tensor(X_test), \
         make_Tensor(y_test)
     # return make_Tensor(x_train), make_Tensor(y_train), make_Tensor(X_val), make_Tensor(y_val), make_Tensor(X_test), \
